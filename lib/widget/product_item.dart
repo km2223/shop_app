ebc7f2ad28deb44ab9cc6bf8439ef874c0b8e473
km2223 <mhmoudalkma22@gmail.com>
2022-11-28 15:35:10 +0300

    Navigator

--- lib/widget/product_item.dart
+++ lib/widget/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shop_app/screens/product_detail_screen.dart';
 
 
 class ProductItem extends StatelessWidget {
@@ -9,13 +10,36 @@ class ProductItem extends StatelessWidget {
  
   @override
   Widget build(BuildContext context) {
-    return GridTile(
-      child:Image.network(
-        imageUrl,
-        fit: BoxFit.cover,) ,
-        footer: GridTileBar(leading: IconButton(onPressed: null, icon: Icon(Icons.favorite)),
-        trailing: IconButton(onPressed: null, icon: Icon(Icons.shopping_cart)),
-        backgroundColor:Colors.black54,title:Text(title,textAlign: TextAlign.center,)),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child:GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: id);
+          },
+          child:  Image.network(
+          imageUrl,
+          fit: BoxFit.cover,) 
+          ),
+          footer: GridTileBar(
+            leading: IconButton(
+              onPressed: (() {
+                
+              }),
+               icon: Icon(Icons.favorite),
+             color:Theme.of(context).colorScheme.onSecondary,
+    
+               ),
+          trailing: IconButton(
+            onPressed: (() {
+              
+            }),
+             icon: Icon(Icons.shopping_cart),
+             ),
+          backgroundColor:Colors.black87,
+          title:Text(title,textAlign: TextAlign.center,)
+          ),
+      ),
     );
   }
 }
